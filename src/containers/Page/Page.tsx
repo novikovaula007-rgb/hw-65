f515365ee75cc17ab35142d88bfafccac75244cd
ulyana <novikova.ula.007@gmail.com>
2026-01-15 20:32:27 +0300

    fetch page function, types

--- src/containers/Page/Page.tsx
+++ src/containers/Page/Page.tsx
@@ -1,7 +1,41 @@
+import {useParams} from "react-router-dom";
+import {useCallback, useEffect, useState} from "react";
+import axiosAPI from "../../axiosAPI.ts";
+import type {IPage} from "../../types";
+
 const Page = () => {
+    const [loading, setLoading] = useState<boolean>();
+    const [currentPage, setCurrentPage] = useState<IPage | null>(null)
+    const {pageName} = useParams();
+
+    const fetchPage = useCallback(async () => {
+        try {
+            setLoading(true);
+
+            let fetchLink = '/pages/home.json'
+            if (pageName) {
+                fetchLink = `/pages/${pageName}.json`
+            }
+
+            const response = await axiosAPI<IPage | null>(fetchLink);
+            const responsePost = response.data;
+            console.log(responsePost, pageName)
+            if (responsePost) {
+                setCurrentPage(responsePost)
+            }
+
+        } catch (e) {
+            console.log(e)
+        } finally {
+            setLoading(false)
+        }
+    }, [pageName])
+    
+    useEffect(() => {
+        void fetchPage()
+    }, [fetchPage])
     return (
         <div>
-
         </div>
     );
 };
