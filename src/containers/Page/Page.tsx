0949d87833a5380f882fac39fa71120091f50d45
ulyana <novikova.ula.007@gmail.com>
2026-01-15 20:41:18 +0300

    spinner and show page content

--- src/containers/Page/Page.tsx
+++ src/containers/Page/Page.tsx
@@ -2,15 +2,19 @@ import {useParams} from "react-router-dom";
 import {useCallback, useEffect, useState} from "react";
 import axiosAPI from "../../axiosAPI.ts";
 import type {IPage} from "../../types";
+import Spinner from "../../components/UI/Spinner/Spinner.tsx";
+import NotFoundPage from "../NotFoundPage/NotFoundPage.tsx";
+import {Box, Typography} from "@mui/material";
 
 const Page = () => {
-    const [loading, setLoading] = useState<boolean>();
+    const [loading, setLoading] = useState<boolean>(false);
     const [currentPage, setCurrentPage] = useState<IPage | null>(null)
     const {pageName} = useParams();
 
     const fetchPage = useCallback(async () => {
         try {
             setLoading(true);
+            setCurrentPage(null)
 
             let fetchLink = '/pages/home.json'
             if (pageName) {
@@ -19,7 +23,6 @@ const Page = () => {
 
             const response = await axiosAPI<IPage | null>(fetchLink);
             const responsePost = response.data;
-            console.log(responsePost, pageName)
             if (responsePost) {
                 setCurrentPage(responsePost)
             }
@@ -30,13 +33,25 @@ const Page = () => {
             setLoading(false)
         }
     }, [pageName])
-    
+
     useEffect(() => {
         void fetchPage()
     }, [fetchPage])
     return (
-        <div>
-        </div>
+        <>
+            {loading && (<Spinner/>)}
+            {!loading && !currentPage && (<NotFoundPage/>)}
+            {!loading && currentPage && (
+                <Box>
+                    <Typography variant='h1'>
+                        {currentPage.title}
+                    </Typography>
+                    <Typography>
+                        {currentPage.content}
+                    </Typography>
+                </Box>
+            )}
+        </>
     );
 };
 
