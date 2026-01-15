4f3e840583ec2fe57f71247c9bc87110e3daf5f6
ulyana <novikova.ula.007@gmail.com>
2026-01-15 19:08:26 +0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import NavBar from "./components/NavBar/NavBar.tsx";
+import {Container} from "@mui/material";
+import {Navigate, Route, Routes} from "react-router-dom";
+import Page from "./containers/Page/Page.tsx";
+import EditPage from "./containers/EditPage/EditPage.tsx";
+
 const App = () => {
   return (
     <>
+      <NavBar/>
+        <Container sx={{marginTop: '30px'}}>
+            <Routes>
+                <Route/>
+                <Route index element={<Navigate to="/pages/home" replace/>}/>
+                <Route path='/pages/admin' element={<EditPage/>}/>
+                <Route path='/pages/:pageName' element={<Page/>}/>
+            </Routes>
+        </Container>
     </>
   )
 }
